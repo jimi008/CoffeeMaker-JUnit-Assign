651d32113ae6fdca11b7f0235e4a989e3272eac2
Jamil Ahmed <jamil.mba4553@iiu.edu.pk>
2023-09-18 20:43:12 +0000

    Fix the tests

--- src/test/java/edu/ncsu/csc326/coffeemaker/CoffeeMakerTest.java
+++ src/test/java/edu/ncsu/csc326/coffeemaker/CoffeeMakerTest.java
@@ -19,8 +19,6 @@
 package edu.ncsu.csc326.coffeemaker;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -31,33 +29,32 @@ import edu.ncsu.csc326.coffeemaker.exceptions.RecipeException;
 /**
  * Unit tests for CoffeeMaker class.
  * 
- * @author Sarah Heckman
+ * @author Jamil Ahmed
  */
 public class CoffeeMakerTest {
-	
+
 	/**
 	 * The object under test.
 	 */
 	private CoffeeMaker coffeeMaker;
-	
+
 	// Sample recipes to use in testing.
 	private Recipe recipe1;
 	private Recipe recipe2;
 	private Recipe recipe3;
 	private Recipe recipe4;
-	private Recipe recipe5;
 
 	/**
-	 * Initializes some recipes to test with and the {@link CoffeeMaker} 
+	 * Initializes some recipes to test with and the {@link CoffeeMaker}
 	 * object we wish to test.
-	 * 
-	 * @throws RecipeException  if there was an error parsing the ingredient 
+	 *
+	 * @throws RecipeException  if there was an error parsing the ingredient
 	 * 		amount when setting up the recipe.
 	 */
 	@Before
 	public void setUp() throws RecipeException {
 		coffeeMaker = new CoffeeMaker();
-		
+
 		//Set up for r1
 		recipe1 = new Recipe();
 		recipe1.setName("Coffee");
@@ -66,7 +63,7 @@ public class CoffeeMakerTest {
 		recipe1.setAmtMilk("1");
 		recipe1.setAmtSugar("1");
 		recipe1.setPrice("50");
-		
+
 		//Set up for r2
 		recipe2 = new Recipe();
 		recipe2.setName("Mocha");
@@ -75,7 +72,7 @@ public class CoffeeMakerTest {
 		recipe2.setAmtMilk("1");
 		recipe2.setAmtSugar("1");
 		recipe2.setPrice("75");
-		
+
 		//Set up for r3
 		recipe3 = new Recipe();
 		recipe3.setName("Latte");
@@ -84,7 +81,7 @@ public class CoffeeMakerTest {
 		recipe3.setAmtMilk("3");
 		recipe3.setAmtSugar("1");
 		recipe3.setPrice("100");
-		
+
 		//Set up for r4
 		recipe4 = new Recipe();
 		recipe4.setName("Hot Chocolate");
@@ -93,24 +90,14 @@ public class CoffeeMakerTest {
 		recipe4.setAmtMilk("1");
 		recipe4.setAmtSugar("1");
 		recipe4.setPrice("65");
-
-		//Set up for r5
-		recipe5 = new Recipe();
-		recipe5.setName("Hot Chocolate");
-		recipe5.setAmtChocolate("4");
-		recipe5.setAmtCoffee("0");
-		recipe5.setAmtMilk("1");
-		recipe5.setAmtSugar("1");
-		recipe5.setPrice("65");		
-
 	}
-	
-	
+
+
 	/**
 	 * Given a coffee maker with the default inventory
 	 * When we add inventory with well-formed quantities
 	 * Then we do not get an exception trying to read the inventory quantities.
-	 * 
+	 *
 	 * @throws InventoryException  if there was an error parsing the quanity
 	 * 		to a positive integer.
 	 */
@@ -118,24 +105,24 @@ public class CoffeeMakerTest {
 	public void testAddInventory() throws InventoryException {
 		coffeeMaker.addInventory("4","7","0","9");
 	}
-	
+
 	/**
 	 * Given a coffee maker with the default inventory
-	 * When we add inventory with malformed quantities (i.e., a negative 
+	 * When we add inventory with malformed quantities (i.e., a negative
 	 * quantity and a non-numeric string)
 	 * Then we get an inventory exception
-	 * 
+	 *
 	 * @throws InventoryException  if there was an error parsing the quanity
 	 * 		to a positive integer.
 	 */
 	@Test(expected = InventoryException.class)
 	public void testAddInventoryException() throws InventoryException {
-		coffeeMaker.addInventory("4", "-1", "asdf", "3");
+		coffeeMaker.addInventory("4", "-1", "-6", "3");
 	}
-	
+
 	/**
 	 * Given a coffee maker with one valid recipe
-	 * When we make coffee, selecting the valid recipe and paying more than 
+	 * When we make coffee, selecting the valid recipe and paying more than
 	 * 		the coffee costs
 	 * Then we get the correct change back.
 	 */
@@ -143,21 +130,34 @@ public class CoffeeMakerTest {
 	public void testMakeCoffee() {
 		coffeeMaker.addRecipe(recipe1);
 		assertEquals(25, coffeeMaker.makeCoffee(0, 75));
-		assertTrue(coffeeMaker.addRecipe(recipe1)); 
-		assertFalse(coffeeMaker.addRecipe(recipe1));
 	}
 
-	/**
-	 * Given a coffee maker with one valid recipe
-	 * When we make coffee, selecting the valid recipe and paying more than 
-	 * 		the coffee costs
-	 * Then we get the correct change back.
-	 */
-	// @Test
-	// public void testaddRecipe() {
-	// 	coffeeMaker.addRecipe(recipe1);
-	// 	assertEquals(25, coffeeMaker.makeCoffee(0, 75));
-	// }
-	
 
+	@Test
+	public void testAddRecipe1()
+	{
+		assertEquals(true,coffeeMaker.addRecipe(recipe1));
+		assertEquals(true,coffeeMaker.addRecipe(recipe2));
+		assertEquals(true,coffeeMaker.addRecipe(recipe3));
+		assertEquals(false,coffeeMaker.addRecipe(recipe4));
+	}
+	@Test
+	public void testMakeCoffee1() {
+		coffeeMaker.addRecipe(recipe1);
+		coffeeMaker.addRecipe(recipe2);
+		coffeeMaker.addRecipe(recipe3);
+		coffeeMaker.addRecipe(recipe4);
+		assertEquals(75, coffeeMaker.makeCoffee(4, 75));
+	}
+	@Test
+	public void testDeleteRecipe4()
+	{   coffeeMaker.addRecipe(recipe1);
+		coffeeMaker.addRecipe(recipe2);
+		coffeeMaker.addRecipe(recipe3);
+		coffeeMaker.addRecipe(recipe4);
+		assertEquals("Coffee",coffeeMaker.deleteRecipe(0));
+		assertEquals("Mocha",coffeeMaker.deleteRecipe(1));
+		assertEquals("Latte",coffeeMaker.deleteRecipe(2));
+		assertEquals(null,coffeeMaker.deleteRecipe(3));
+	}
 }
